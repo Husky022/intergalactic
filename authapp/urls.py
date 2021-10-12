d18b92d00c2f8f72b4b68bcdc700d5600f1b2dd4
Sin93 <zinovev.maxim93@mail.ru>
2021-10-12 23:12:28 +0300

    переписал вьюху под cbv

--- authapp/urls.py
+++ authapp/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('logout/', authapp.LogoutView.as_view(), name='logout'),
     path('register/', authapp.RegisterView.as_view(), name='register'),
     path('edit/', authapp.UserEditView.as_view(), name='edit'),
-    path('profile/', authapp.user_profile, name='profile')
+    path('profile/', authapp.UserProfileView.as_view(), name='profile')
 ]
