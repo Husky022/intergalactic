b60f169a300d86c067b9752302faf0a030df35bd
Groofidze <groofi@list.ru>
2021-10-13 23:19:49 +0300

    fix_database

--- intergalactic/settings.py
+++ intergalactic/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from intergalactic.secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qs*o^!)t$x6@)h07a-co7o%*tx@^cy(619a-3)c*y$v!^m1kyf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
