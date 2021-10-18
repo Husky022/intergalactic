27919cce4261f651990e29e0c104a8bd7ae8f816
pavlmir <mirprost@gmail.com>
2021-10-18 23:37:44 +0300

    IE - 124 добавлен функционал изменения пользователей

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import HttpResponseRedirect, get_object_or_404, redirect, render
+from django.urls import reverse
+from django.views.generic.list import ListView
 
-# Create your views here.
+from adminapp.forms import IntergalacticUserAdminEditForm
+from authapp.forms import IntergalacticUserRegisterForm
+from authapp.models import IntergalacticUser
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_main(request):
+    response = redirect('adminapp:users')
+    return response
+
+
+class UsersListView(LoginRequiredMixin, ListView):
+    model = IntergalacticUser
+    template_name = "adminapp/users.html"
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def user_create(request):
+    title = "пользователи/создание"
+
+    if request.method == "POST":
+        user_form = IntergalacticUserRegisterForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse("admin:users"))
+    else:
+        user_form = IntergalacticUserRegisterForm()
+
+    content = {"title": title, "update_form": user_form, "media_url": settings.MEDIA_URL}
+
+    return render(request, "adminapp/user_update.html", content)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def user_update(request, pk):
+    title = "пользователи/редактирование"
+
+    edit_user = get_object_or_404(IntergalacticUser, pk=pk)
+    if request.method == "POST":
+        edit_form = IntergalacticUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse("admin:user_update", args=[edit_user.pk]))
+    else:
+        edit_form = IntergalacticUserAdminEditForm(instance=edit_user)
+
+    content = {"title": title, "update_form": edit_form, "media_url": settings.MEDIA_URL}
+
+    return render(request, "adminapp/user_update.html", content)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def user_delete(request, pk):
+    title = "пользователи/удаление"
+
+    user = get_object_or_404(IntergalacticUser, pk=pk)
+
+    if request.method == "POST":
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse("admin:users"))
+
+    content = {"title": title, "user_to_delete": user, "media_url": settings.MEDIA_URL}
+
+    return render(request, "adminapp/user_delete.html", content)
+
+
+def db_profile_by_type(prefix, type, queries):
+    update_queries = list(filter(lambda x: type in x["sql"], queries))
+    print(f"db_profile {type} for {prefix}:")
+    [print(query["sql"]) for query in update_queries]
