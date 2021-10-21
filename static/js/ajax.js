b306cd25b8cbf079f63de78a11db552a5894acbc
Groofidze <groofi@list.ru>
2021-10-21 04:40:45 +0300

    comments_bug_fix

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,10 +1,22 @@
+function show(id){
+    elem = document.getElementById(id);
+    state = elem.style.display;
+    if (state =='block')
+        elem.style.display='none';
+    else
+        elem.style.display='block';
+}
+
 window.onload = function () {
+
+
+
     $('.ajax_comment').on('click', '.submit_comment', function () {
         let target_href = event.target;
         if (target_href) {
             $.ajax({
                 url: "/article_page/" + target_href.name + "/",
-                data: {  text_comment: $('textarea').val()},
+                data: {text_comment: $('.textarea').val()},
                 success: function (data) {
                     $('.comment-main').remove();
                     $('.ajax_comment').html(data.result);
@@ -15,6 +27,7 @@ window.onload = function () {
         }
         event.preventDefault();
     });
+
     $(".ajax_like").on('click', '.btn-likes', function(event){
             $.ajax({
                 url: "/set_like/" + event.target.id + '/',
@@ -29,5 +42,26 @@ window.onload = function () {
                 }
             });
     })
+
+    $('.ajax_comment').on('click', '.submit_subcomment', function () {
+        let target_href = event.target;
+        if (target_href) {
+            $.ajax({
+                url: "/article_page/" + target_href.id + "/",
+                data: {  comment_id: $('.submit_subcomment_' + target_href.value).val(),
+                         text_subcomment: $('.textarea_subcomment_' + target_href.value).val(),
+                      },
+                success: function (data) {
+                    $('.comment-main').remove();
+                    $('.ajax_comment').html(data.result);
+
+                },
+            });
+
+        }
+        event.preventDefault();
+    });
+
+
 
 };
--- static/js/sub_comments_show.js
+++
@@ -1,8 +0,0 @@
-function show(id){
-    elem = document.getElementById(id);
-    state = elem.style.display;
-    if (state =='block')
-        elem.style.display='none';
-    else
-        elem.style.display='block';
-}
