b73844bec320245e5250d1d9217a6dee90857675
Ilya <balashovia@bk.ru>
2021-10-26 19:54:07 +0300

    IE-114_continue: Правка верстки, добавил модель Уведомлений

--- authapp/urls.py
+++ authapp/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('logout/', authapp.LogoutView.as_view(), name='logout'),
     path('register/', authapp.RegisterView.as_view(), name='register'),
     path('edit/', authapp.UserEditView.as_view(), name='edit'),
-    path('profile/', authapp.UserProfileView.as_view(), name='profile')
+    path('profile/', authapp.UserProfileView.as_view(), name='profile'),
+    path('notifications/', authapp.NotificationView.as_view(), name='notifications')
 ]
