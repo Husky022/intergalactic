826085b27914baea272da91b9b1000bbfb54db48
Groofidze <groofi@list.ru>
2021-10-28 00:39:24 +0300

    fix_pavel_sobolev-v0.5

--- mainapp/services/activity/render_context.py
+++ mainapp/services/activity/render_context.py
@@ -8,6 +8,7 @@ from mainapp.services.activity.comment import CommentSubcomment
 from mainapp.models import Article, Comment, Likes, SubComment
 
 
+# Article View
 class RenderArticle:
     """Рендер контекста Article и Activity в множественном количестве"""
 
@@ -59,40 +60,7 @@ class Parse:
         return cls.types[type_](*args)
 
 
-# def parse_all():
-#     return Article.objects.filter(is_active=True)
-#
-#
-# def parse_filter(self):
-#     return Article.objects.filter(hub__id=self.kwargs['pk'], is_active=True)
-#
-#
-# def add_item(item, comment_list):
-#     item.item_comment = Comment.objects.filter(article=item, is_active=True).count() + SubComment.objects.filter(
-#         article=item, is_active=True).count()
-#     item.like_count = Likes.objects.filter(article=item, status="LK").count()
-#     item.dislike_count = Likes.objects.filter(article=item, status="DZ").count()
-#     comment_list.append(item)
-#
-#
-# def for_parse(type_, comment_list, *args):
-#     for item in Parse.create(type_, *args):
-#         add_item(item, comment_list)
-#
-#
-# def if_article(comment_list, self):
-#     if self.kwargs.get("pk", None):
-#         for_parse("filter", comment_list, self)
-#     else:
-#         for_parse("all", comment_list)
-#
-#
-# def queryset_activity(self):
-#     comment_list = []
-#     if_article(comment_list, self)
-#     return comment_list
-
-
+# ArticlePage View
 def fill_context(self):
     """Рендер контекста"""
     self.object = self.get_object()
