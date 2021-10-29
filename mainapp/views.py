8634b9e90b611024b12257513adbbfd28d4d22f2
Groofidze <groofi@list.ru>
2021-10-29 23:47:40 +0300

    Fix_Pagination-v0.1

--- mainapp/views.py
+++ mainapp/views.py
@@ -122,7 +122,6 @@ class ArticlePage(DetailView):
         return HttpResponseRedirect(reverse_lazy('article_page', args=(int(self.kwargs["pk"]),)))
 
 
-
 class ArticleCreationView(CreateView):
     """CBV для создание статьи"""
     model = Article
@@ -221,26 +220,29 @@ class DraftArticle(View):
 class Search(ListView):
     model = Article
     template_name = 'mainapp/articles.html'
-    extra_context = {'title': 'Статьи'}
+    extra_context = {'title': 'Поиск'}
     paginate_by = 5
 
     def get(self, request, page_num=1, *args, **kwargs):
         article = Article.objects.filter(article_status='PB')
         search_filter = ArticleFilter(request.GET, queryset=article)
         article = search_filter.qs
+        self.object_list = article
+        allow_empty = self.get_allow_empty()
 
-        # paginator = Paginator(article, 2)
-        # page = request.GET.get('page')
-        #
-        # try:
-        #     article = paginator.page(page)
-        # except PageNotAnInteger:
-        #     article = paginator.page(1)
-        # except EmptyPage:
-        #     article = paginator.page(paginator.num_pages)
-        context = {'page_title': 'Поиск',
-                  'object_list': article,
-                  'search_filter': search_filter,
-                  # 'page_obj': article
-                  }
+        if not allow_empty:
+            # When pagination is enabled and object_list is a queryset,
+            # it's better to do a cheap query than to load the unpaginated
+            # queryset in memory.
+            if self.get_paginate_by(self.object_list) is not None and hasattr(self.object_list, 'exists'):
+                is_empty = not self.object_list.exists()
+            else:
+                is_empty = not self.object_list
+            if is_empty:
+                raise Http404(_('Empty list and “%(class_name)s.allow_empty” is False.') % {
+                    'class_name': self.__class__.__name__,
+                })
+        context = self.get_context_data()
+        context['search_filter'] = search_filter
+        context['filter'] = request.GET.dict()
         return render(request, 'mainapp/articles.html', context)
