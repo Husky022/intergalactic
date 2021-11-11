887e62bb66f68c2905a5da3b6097daf42e5df8f4
serzh251 <serzh251@mail.ru>
2021-11-11 13:54:42 +0300

    index page

--- mainapp/views.py
+++ mainapp/views.py
@@ -18,28 +18,17 @@ from .services.sorting import get_sorted_queryset
 class Main(ListView):
     """ CBV Главной страницы """
     model = Article
-    template_name = 'mainapp/articles.html'
-    paginate_by = 5
+    template_name = 'mainapp/index.html'
     extra_context = {'title': 'Главная'}
 
-    def get_queryset(self):
-        article = Article.objects.all()
-        return get_sorted_queryset(self, article)
-
     def get(self, request, *args, **kwargs):
-        self.object_list = self.get_queryset()
-        allow_empty = self.get_allow_empty()
+        article_all = self.get_queryset()
+        self.object_list = article_all
 
-        if not allow_empty:
-            if self.get_paginate_by(self.object_list) is not None and hasattr(self.object_list, 'exists'):
-                is_empty = not self.object_list.exists()
-            else:
-                is_empty = not self.object_list
-            if is_empty:
-                raise Http404(_('Empty list and “%(class_name)s.allow_empty” is False.') % {
-                    'class_name': self.__class__.__name__,
-                })
         context = self.get_context_data()
+        context['top_news'] = article_all.order_by('-add_datetime')[:3]
+        context['top_popular'] = article_all.order_by('-rating')[:3]
+        context['top_views'] = article_all.order_by('-views')[:3]
         return self.render_to_response(context)
 
 
