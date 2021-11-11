27da441d7ea47bd8ab5cf5319db6e88f1c5e7f82
Groofidze <groofi@list.ru>
2021-11-11 06:10:03 +0300

    IE-219_v1.01

--- mainapp/views.py
+++ mainapp/views.py
@@ -24,24 +24,9 @@ class Main(ListView):
 
     def get_queryset(self):
         status = ArticleStatus.objects.get(name='Опубликована')
-        article = Article.objects.all(article_status_new=status)
+        article = Article.objects.filter(article_status_new=status)
         return get_sorted_queryset(self, article)
 
-    def get(self, request, *args, **kwargs):
-        self.object_list = self.get_queryset()
-        allow_empty = self.get_allow_empty()
-
-        if not allow_empty:
-            if self.get_paginate_by(self.object_list) is not None and hasattr(self.object_list, 'exists'):
-                is_empty = not self.object_list.exists()
-            else:
-                is_empty = not self.object_list
-            if is_empty:
-                raise Http404(_('Empty list and “%(class_name)s.allow_empty” is False.') % {
-                    'class_name': self.__class__.__name__,
-                })
-        context = self.get_context_data()
-        return self.render_to_response(context)
 
 
 class Articles(ListView):
@@ -61,21 +46,6 @@ class Articles(ListView):
 
         return get_sorted_queryset(self, article)
 
-    def get(self, request, *args, **kwargs):
-        self.object_list = self.get_queryset()
-        allow_empty = self.get_allow_empty()
-
-        if not allow_empty:
-            if self.get_paginate_by(self.object_list) is not None and hasattr(self.object_list, 'exists'):
-                is_empty = not self.object_list.exists()
-            else:
-                is_empty = not self.object_list
-            if is_empty:
-                raise Http404(_('Empty list and “%(class_name)s.allow_empty” is False.') % {
-                    'class_name': self.__class__.__name__,
-                })
-        context = self.get_context_data()
-        return self.render_to_response(context)
 
 
 class ArticlePage(DetailView):
