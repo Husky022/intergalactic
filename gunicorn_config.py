dff3c23d3be38c4dd6fcdc34ab034d5717b6a4f4
Pavel <varchikcreed@mail.ru>
2021-11-15 10:07:58 +0300

    network-v0.4

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,8 @@
-command = '/home/django/proj/venv_ie/bin/gunicorn'
-pythonpath = '/home/django/proj/intergalactic'
+command = '/home/intergalactic/project/django/env/bin/gunicorn'
+pythonpath = '/home/intergalactic/project/django'
 bind = '127.0.0.1:8001'
 workers = 3
-user = 'django'
+user = 'intergalactic'
 limit_request_fields = 32000
 limit_request_field_size = 0
 raw_env = 'DJANGO_SETTINGS_MODULE=intergalactic.settings'
