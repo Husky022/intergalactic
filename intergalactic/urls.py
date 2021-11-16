387818ec6944243850a6e7d7d761f2272fa05950
Sin93 <zinovev.maxim93@mail.ru>
2021-11-16 23:39:55 +0300

    Создание чата, написание сообщений

--- intergalactic/urls.py
+++ intergalactic/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 
+
 urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('', include('mainapp.urls')),
