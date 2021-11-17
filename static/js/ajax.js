06449975284555ac3cc107385ff3198044ed37be
Ilya <balashovia@bk.ru>
2021-11-17 19:44:46 +0300

    IE-226.1 немного подправил JS, иначе возникала ошибка

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,10 +1,11 @@
 function show(id) {
     elem = document.getElementById(id);
     state = elem.style.display;
-    if (state == 'block')
-        elem.style.display = 'none';
-    else
-        elem.style.display = 'block';
+    if (state == 'block') {
+        elem.style.display = 'none'
+    } else {
+        elem.style.display = 'block'
+    }
 }
 
 function likesDislikeComment(data, btn, id) {
